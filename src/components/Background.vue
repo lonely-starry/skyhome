7ca3b8cf68edb1adc0695b8c0861d177fc9764c6
lonely-starry <lonely-x@outlook.com>
2024-03-11 17:40:35 +0800

    修复壁纸接口参数

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -38,14 +38,14 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 
 // 更换壁纸链接
 const changeBg = (type) => {
-  if (type == 4) {
-    bgUrl.value = `/images/background${bgRandom}.jpg`;
+  if (type == 0) {
+    bgUrl.value = "https://api.lonelyx.cn/api/";
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
     bgUrl.value = "https://api.aixiaowai.cn/gqapi/gqapi.php";
-  } else if (type == 0) {
-    bgUrl.value = "https://api.lonelyx.cn/api/";
+  } else if (type == 3) {
+    bgUrl.value = `/images/background${bgRandom}.jpg`;
   }
 };
 
