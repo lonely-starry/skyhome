f89259233d545ea4889c867d52a0cda92e99b094
lonely-starry <1741713961@qq.com>
2024-03-12 13:01:36 +0800

    更新控制台参数和部分代码

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,7 @@ onMounted(() => {
   // 屏蔽右键
   document.oncontextmenu = () => {
     ElMessage({
-      message: "为了浏览体验，本站禁用右键",
+      message: "本站禁用右键",
       grouping: true,
       duration: 2000,
     });
@@ -107,7 +107,7 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "無名の主页";
+  const title1 = "彼岸の星空";
   const title2 = `
  _____ __  __  _______     ____     __
 |_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
